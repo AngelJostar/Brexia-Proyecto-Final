08940e2f6d1f6d868e5538affc0eae463ed3b40f
AngelJostar <angelrojas@ciencias.unam.mx>
2020-10-28 22:34:14 -0600

    commit izquierda y derecha terminado

--- js/scripts.js
+++ js/scripts.js
@@ -44,8 +44,7 @@ $(document).ready(function() {
     // Get the header
     var header = document.getElementById("myHeader");
 
-    // Get the offset position of the navbar
-    var sticky = header.offsetTop;
+
 
     // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction() {
