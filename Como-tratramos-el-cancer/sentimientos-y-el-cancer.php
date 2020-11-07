be97e7f48911875309e554580d7d87818e3d9d5b
AngelJostar <angelrojas@ciencias.unam.mx>
2020-11-07 01:53:54 -0600

    commit footer-añadido

--- Como-tratramos-el-cancer/sentimientos-y-el-cancer.php
+++ Como-tratramos-el-cancer/sentimientos-y-el-cancer.php
@@ -22,86 +22,7 @@
         <!-- CANVAS -->
         <section id="site-canvas">
 
-            <header>
-                <div class="contenedor">
-                    <div class="mid-header">
-                        <div class="mid-image deepmid-header">
-                            <img src="../img/logo-Brexia-con-colores-(2).png" alt="imagen-logo ">
-                        </div>
-                        <div class="mid-font">
-                            <p class="centrar-texto fontW-700">Llámanos, estamos disponibles <br> tel: (55) 5523 8959</p>
-                        </div>
-                        <div class="mid-font">
-                            <p class="centrar-texto fontW-700">Escríbenos por Whatsapp <br> <a href="#">(celular): 52 (1) 55 5135 8125</a></p>
-                        </div>
-                        <div class="icono">
-                            <div data-wow-delay="0.1s">
-                                <div>
-                                    <ul class="company-social">
-                                        <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f fa-2x"></i></a></li>
-                                        <li class="social-youtube"><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
-                                        <li class="social-whatsapp"><a href="#"><i class="fab fa-whatsapp fa-2x"></i></a></li>
-                                        <li class="social-instagram"><a href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
-                                        <li class="social-twitter"><a href="#"><i class="fab fa-twitter fa-2x"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- MENU -->
-                        <div id="site-menu">
-                            <div class="site-menu-content">
-                                <a href="#" class="toggle-nav icono-menu"><i class="fas fa-times"></i></a>
-                                <ul class="resaltar">
-                                    <li><a href="../index.html" class="menu-a">Inicio</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../como-tratamos-el-cancer.html" class="menu-a">¿Cómo tratamos el Cáncer?</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../ser-paciente.html" class="menu-a">Ser paciente en Brexia</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../aspectos-basicos-cancer.html" class="menu-a">Aspectos básicos del Cáncer</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../tipos-de-cancer.html" class="menu-a">Tipos de Cáncer</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../segundo-opinion.html" class="menu-a">Segunda Opinión</a></li>
-                                    <div class="linea-menu"></div>
-                                    <li><a href="../contacto.html" class="menu-a">Contacto</a></li>
-                                    <div class="linea-menu"></div>
-                                </ul>
-                                <div class="centrar-texto">
-                                    <button class="boton-brexia-menu">Llámanos <br> (55) 5523 8959</button>
-                                </div>
-                                <div class="centrar-texto">
-                                    <button class="boton-brexia-menu">Escribenos a <br> contacto@brexia.mx</button>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- CANVAS CONTENT -->
-                        <div class="contentido deepmid-header">
-                            <!-- MENU TOGGLE -->
-                            <div class="justificar">
-                                <a href="#" class="toggle-nav nav-icon"><i class="fa fa-bars color-brexia"></i></a>
-                                <p class="menu-literal">MENU</p>
-                                <!-- CONTENT -->
-                            </div>
-                        </div>
-                        <!-- END CANVAS CONTENT -->
-                    </div>
-
-
-                    <div class="low-header">
-                        <ul>
-                            <li><a href="../index.html ">Inicio</a></li>
-                            <li><a href="../como-tratamos-el-cancer.html">¿Cómo tratamos el Cáncer?</a></li>
-                            <li><a href="../ser-paciente.html">Ser paciente en Brexia</a></li>
-                            <li><a href="../aspectos-basicos-cancer.html">Aspectos básicos del Cáncer</a></li>
-                            <li><a href="../tipos-de-cancer.html">Tipos de Cáncer</a></li>
-                            <li><a href="../segundo-opinion.html">Segunda Opinión</a></li>
-                            <li><a href="../contacto.html">Contacto</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-            </header>
+        <?php include_once '../includes/templates/header_tipos.php';?>
 
             <section>
                 <div class="fondo-tipos-de-cancer ">
@@ -110,6 +31,12 @@
                     </div>
                     <div>
                         <img src="/img/sentimientos-y-el-cancer/sentimientos-y-cancer-Brexia.webp" alt="">
+                        <div class="tipos-cancer-regresar">
+                            <a href="../tipos-de-cancer.php">
+                            &nbsp; << OTROS TIPOS DE CÁNCER</a>
+                            <a href="../contacto.php">
+                            &nbsp; CONTACTANOS >></a>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -413,49 +340,7 @@
                 <button class="boton boton-brexia-footer">Regresar al inicio</button>
             </div>
 
-            <!--####FOOTER####-->
-            <footer class="relativo">
-                <div class="linea"></div>
-                <div class="contenedor footer-grid">
-                    <div class="contenedor-footer">
-                        <div class="footer-grid">
-                            <div class="centrar-texto llamanos-footer">
-                                <a href="#"> <strong>Llámanos ahora:</strong> <br class="br-footer"> (55) 5523 8959</a>
-                            </div>
-                            <div class="centrar-texto">
-                                <button class="boton boton-brexia-footer">Contacto</button>
-                            </div>
-                        </div>
-                        <div class="centrar-texto text-footer">
-                            <p class="paddingTop-3 paddingBottom-3">San Francisco 524, Col. Del Valle CDMX, México</p>
-                        </div>
-                    </div>
-                    <div class="contenedor-footer">
-                        <div class="centrar-texto text-footer ocultar-text">
-                            <p>El cáncer en ningún caso se debe de auto diagnosticar ni auto medicar. </p>
-                        </div>
-                        <div class="centrar-texto">
-                            <div class="redes-footer">
-                                <div class="wow fadeInDown" data-wow-delay="0.1s">
-                                    <div class="widget">
-                                        <ul class="company-social">
-                                            <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f fa-2x"></i></a></li>
-                                            <li class="social-youtube"><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
-                                            <li class="social-whatsapp"><a href="#"><i class="fab fa-whatsapp fa-2x"></i></a></li>
-                                            <li class="social-instagram"><a href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
-                                            <li class="social-twitter"><a href="#"><i class="fab fa-twitter fa-2x"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="deepfooter">
-                    <p>Aviso de Privacidad/Mapa de Sitio/ Permiso COFEPRIS: 193300201A0691 / 193300201A0861</p>
-                </div>
-
-            </footer>
+            <?php include_once '../includes/templates/footer.php';?>
 
 
 
--- gracias.php
+++ gracias.php
@@ -36,49 +36,7 @@
             </section>
 
 
-            <!--####FOOTER####-->
-            <footer class="relativo">
-                <div class="linea"></div>
-                <div class="contenedor footer-grid">
-                    <div class="contenedor-footer">
-                        <div class="footer-grid">
-                            <div class="centrar-texto llamanos-footer">
-                                <a href="#"> <strong>Llámanos ahora:</strong> <br class="br-footer"> (55) 5523 8959</a>
-                            </div>
-                            <div class="centrar-texto">
-                                <button class="boton boton-brexia-footer">Contacto</button>
-                            </div>
-                        </div>
-                        <div class="centrar-texto text-footer">
-                            <p class="paddingTop-3 paddingBottom-3 fontW-400">San Francisco 524, Col. Del Valle CDMX, México</p>
-                        </div>
-                    </div>
-                    <div class="contenedor-footer">
-                        <div class="centrar-texto text-footer ocultar-text ">
-                            <p class="fontW-400">El cáncer en ningún caso se debe de auto diagnosticar ni auto medicar. </p>
-                        </div>
-                        <div class="centrar-texto">
-                            <div class="redes-footer">
-                                <div class="wow fadeInDown" data-wow-delay="0.1s">
-                                    <div class="widget">
-                                        <ul class="company-social">
-                                            <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f fa-2x"></i></a></li>
-                                            <li class="social-youtube"><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
-                                            <li class="social-whatsapp"><a href="#"><i class="fab fa-whatsapp fa-2x"></i></a></li>
-                                            <li class="social-instagram"><a href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
-                                            <li class="social-twitter"><a href="#"><i class="fab fa-twitter fa-2x"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="deepfooter">
-                    <p> Aviso de Privacidad/Mapa de Sitio/ Permiso COFEPRIS: 193300201A0691 / 193300201A0861</p>
-                </div>
-
-            </footer>
+            <?php include_once 'includes/templates/footer.php';?>
 
 
         </section>
