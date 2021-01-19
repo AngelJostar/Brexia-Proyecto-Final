8afe809ac28f1af142f4c5a258336b3576d96db9
AngelJostar <angelrojas@ciencias.unam.mx>
2021-01-18 22:19:24 -0600

    commit correciones 4

--- aspectos-basicos-cancer.php
+++ aspectos-basicos-cancer.php
@@ -3,105 +3,107 @@
 
 <?php include_once 'includes/tags/google_tag_manager_head.php';?>
 <link rel="icon" type="image/png" href="img/cropped-favicon-1-32x32.png" sizes="32x32">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="format-detection" content="telephone=no">
-    <title>El cáncer</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>El cáncer</title>
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <?php include_once 'includes/tags/google_tag_manager_body.php';?>
-    <div id="top-header" class="top-header ">
-         <a href="tel:+5555238959"><p>Llámanos: (55) 5523 8959</p></a>
-    </div>
-    <!--####HEADER####-->
-    <section id="site-wrapper">
-        <!-- CANVAS -->
-        <section id="site-canvas">
+<div id="top-header" class="top-header ">
+    <a href="tel:+5555238959">
+        <p>Llámanos: (55) 5523 8959</p>
+    </a>
+</div>
+<!--####HEADER####-->
+<section id="site-wrapper">
+    <!-- CANVAS -->
+    <section id="site-canvas">
 
-            <?php include_once 'includes/templates/header.php';?>
+        <?php include_once 'includes/templates/header.php';?>
 
 
-            <section id="intro-content" class="intro-content">
-                <img src="img/especialistas.jpg" alt="Doctores">
-                <div class="tipos-cancer-regresar">
-                    <a href="../index ">
+        <section id="intro-content" class="intro-content">
+            <img src="img/especialistas.jpg" alt="Doctores">
+            <div class="tipos-cancer-regresar">
+                <a href="../index ">
                         &nbsp; << INICIO</a>
-                    <a href="../contacto ">
+                <a href="../contacto ">
                         &nbsp; CONTACTANOS >></a>
-                </div>
-            </section>
+            </div>
+        </section>
 
-            <div class="contenedor-2 justificar-texto texto-brexia">
-                <h2 class="formato-h2 centrar-texto paddingTop-1 ">Cáncer Aspectos Básicos</h2>
-                <p>En Brexia, nuestra promesa para usted y su familia es ofrecer información clara. Es por eso que estamos comprometidos a educar a los pacientes y a sus familias para ayudarlos a tomar decisiones de tratamiento informadas para ayudarlo a
-                    usted y a su familia a decidir a dónde ir para recibir tratamiento, como parte de muchos otros factores que pueden estar considerando. Haga click en el enlace a continuación para encontrar toda la información relacionada con ASPECTOS
-                    BÁSICOS DEL CÁNCER.
-                </p>
-                <div class="centrar-texto pt-4">
+        <div class="contenedor-2 justificar-texto texto-brexia">
+            <h2 class="formato-h2 centrar-texto paddingTop-1 ">Cáncer Aspectos Básicos</h2>
+            <p>En Brexia, nuestra promesa para usted y su familia es ofrecer información clara. Es por eso que estamos comprometidos a educar a los pacientes y a sus familias para ayudarlos a tomar decisiones de tratamiento informadas para ayudarlo a usted
+                y a su familia a decidir a dónde ir para recibir tratamiento, como parte de muchos otros factores que pueden estar considerando. Haga click en el enlace a continuación para encontrar toda la información relacionada con ASPECTOS BÁSICOS
+                DEL CÁNCER.
+            </p>
+            <div class="centrar-texto pt-4">
                 <a href="Como-tratramos-el-cancer/Informacion-del-Cancer"><button class="boton boton-brexia"> Información del Cáncer</button></a>
-                </div>
-
-                <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Cómo se diagnostica el cáncer?</h2>
-                <p>Las pruebas y los procedimientos que usamos en Brexia para diagnosticar el cáncer varían según el tipo de enfermedad y las necesidades de cada paciente. Haga click en el enlace a continuación para encontrar toda la información relacionada
-                    con el DIAGNÓSTICO DEL CÁNCER.
-                </p>
-
-                <div class="centrar-texto pt-4">
-                    <a href="Como-tratramos-el-cancer/Diagnosticando-el-Cancer"><button class="boton boton-brexia"> Diagnosticando el Cáncer</button></a>
-                </div>
-                <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Cómo se trata el cáncer?</h2>
-                <p>En Brexia nuestros expertos en cáncer tratan el cáncer con una amplia gama de técnicas y enfoques innovadores que incluyen herramientas de diagnóstico de alta tecnología, radioterapia dirigida, técnicas quirúrgicas mínimamente invasivas
-                    y tratamientos de medicina de precisión. Haga click en el enlace a continuación para encontrar toda la información relacionada con el TRATAMIENTO DEL CÁNCER.
-                </p>
-
-                <div class="centrar-texto paddingTop-2">
-                    <a href="Como-tratramos-el-cancer/Opciones-de-tratamiento"><button class="boton boton-brexia">Opciones de Tratamiento</button></a>
-                </div>
-                <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Quién trata el cáncer?</h2>
-                <p>En Brexia tratamos el cáncer con un enfoque multidisciplinario que junta tratamientos convencionales con terapias de apoyo, administrados simultáneamente por un equipo colaborativo de médicos. El objetivo de la atención integral del cáncer
-                    es ayudar a los pacientes a tolerar mejor los efectos secundarios relacionados con el tratamiento para aprovechar al máximo la vida. Haga click en el enlace a continuación para encontrar toda la información relacionada con todos LOS
-                    ESPECIALISTAS INVOLUCRADOS EN EL TRATAMIENTO DEL CÁNCER.
-                </p>
-
-                <div class="centrar-texto paddingTop-2">
-                    <a href="Como-tratramos-el-cancer/Equipo-multidisciplinario"><button class="boton boton-brexia">Equipo multidisciplinario</button></a>
-                </div>
-
-                <h2 class="formato-h2 centrar-texto paddingTop-1 ">Sentimientos y Emociones</h2>
-                <p>Para los pacientes con cáncer el apoyo psicológico puede desempeñar un papel fundamental en su viaje de tratamiento.En Brexia cuidamos los sentimientos y emociones de los pacientes y sus familiares para que puedan encontrar la fuerza,
-                    la motivación y la inspiración que necesitan durante su viaje por el cáncer. Haga click en el enlace a continuación para encontrar toda la información relacionada con LOS SENTIMIENTOS Y EL CÁNCER.
-                </p>
-
-                <div class="centrar-texto paddingTop-2 paddingBottom-3">
-                    <a href="Como-tratramos-el-cancer/Sentimientos-y-el-cancer"><button class="boton boton-brexia">Sentimientos y el Cáncer</button></a>
-                </div>
-
             </div>
 
+            <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Cómo se diagnostica el cáncer?</h2>
+            <p>Las pruebas y los procedimientos que usamos en Brexia para diagnosticar el cáncer varían según el tipo de enfermedad y las necesidades de cada paciente. Haga click en el enlace a continuación para encontrar toda la información relacionada
+                con el DIAGNÓSTICO DEL CÁNCER.
+            </p>
 
-            <?php include_once 'includes/templates/footer.php';?>
+            <div class="centrar-texto pt-4">
+                <a href="Como-tratramos-el-cancer/Diagnosticando-el-Cancer"><button class="boton boton-brexia"> Diagnosticando el Cáncer</button></a>
+            </div>
+            <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Cómo se trata el cáncer?</h2>
+            <p>En Brexia nuestros expertos en cáncer tratan el cáncer con una amplia gama de técnicas y enfoques innovadores que incluyen herramientas de diagnóstico de alta tecnología, radioterapia dirigida, técnicas quirúrgicas mínimamente invasivas y
+                tratamientos de medicina de precisión. Haga click en el enlace a continuación para encontrar toda la información relacionada con el TRATAMIENTO DEL CÁNCER.
+            </p>
+
+            <div class="centrar-texto paddingTop-2">
+                <a href="Como-tratramos-el-cancer/Opciones-de-tratamiento"><button class="boton boton-brexia">Opciones de Tratamiento</button></a>
+            </div>
+            <h2 class="formato-h2 centrar-texto paddingTop-1 ">¿Quién trata el cáncer?</h2>
+            <p>En Brexia tratamos el cáncer con un enfoque multidisciplinario que junta tratamientos convencionales con terapias de apoyo, administrados simultáneamente por un equipo colaborativo de médicos. El objetivo de la atención integral del cáncer
+                es ayudar a los pacientes a tolerar mejor los efectos secundarios relacionados con el tratamiento para aprovechar al máximo la vida. Haga click en el enlace a continuación para encontrar toda la información relacionada con todos LOS ESPECIALISTAS
+                INVOLUCRADOS EN EL TRATAMIENTO DEL CÁNCER.
+            </p>
+
+            <div class="centrar-texto paddingTop-2">
+                <a href="Como-tratramos-el-cancer/Equipo-multidisciplinario"><button class="boton boton-brexia">Equipo Multidisciplinario</button></a>
+            </div>
+
+            <h2 class="formato-h2 centrar-texto paddingTop-1 ">Sentimientos y Emociones</h2>
+            <p>Para los pacientes con cáncer el apoyo psicológico puede desempeñar un papel fundamental en su viaje de tratamiento.En Brexia cuidamos los sentimientos y emociones de los pacientes y sus familiares para que puedan encontrar la fuerza, la motivación
+                y la inspiración que necesitan durante su viaje por el cáncer. Haga click en el enlace a continuación para encontrar toda la información relacionada con LOS SENTIMIENTOS Y EL CÁNCER.
+            </p>
+
+            <div class="centrar-texto paddingTop-2 paddingBottom-3">
+                <a href="Como-tratramos-el-cancer/Sentimientos-y-el-cancer"><button class="boton boton-brexia">Sentimientos y el Cáncer</button></a>
+            </div>
+
+        </div>
 
 
-        </section>
-        <!-- END SITE CONTENT -->
+        <?php include_once 'includes/templates/footer.php';?>
+
 
     </section>
-    <!--####HEADER####-->
+    <!-- END SITE CONTENT -->
 
-    <a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
-    <!--####FOOTER####-->
+</section>
+<!--####HEADER####-->
+
+<a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
+<!--####FOOTER####-->
 
 
-    <script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script>
-    <script src="../js/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script>
+<script src="../js/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-    <!-- ### EDITABLE JS/JQUERY INCLUDE ### -->
-    <script src="../js/scripts.js "></script>
+<!-- ### EDITABLE JS/JQUERY INCLUDE ### -->
+<script src="../js/scripts.js "></script>
 
 </body>
 
