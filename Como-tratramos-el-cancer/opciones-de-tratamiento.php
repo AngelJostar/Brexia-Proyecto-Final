cd94a675ce32a63d60b7d28c427af27a64a7efea
AngelJostar <angelrojas@ciencias.unam.mx>
2021-07-27 17:11:08 -0500

    commit all

--- Como-tratramos-el-cancer/opciones-de-tratamiento.php
+++ Como-tratramos-el-cancer/opciones-de-tratamiento.php
@@ -34,9 +34,9 @@
                     <div>
                         <img src="/img/opciones-de-tratamiento/Portada-Tratamiento.webp" alt="">
                         <div class="tipos-cancer-regresar">
-                            <a href="../tipos-de-cancer.php">
+                            <a href="../tipos-de-cancer">
                             &nbsp; << OTROS TIPOS DE CÁNCER</a>
-                            <a href="../contacto.php">
+                            <a href="../contacto">
                             &nbsp; CONTACTANOS >></a>
                         </div>
                     </div>
